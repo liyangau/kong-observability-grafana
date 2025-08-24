4113a567002d67529dc068fce3bcf0993f6e44de
liyangau <d@aufomm.com>
2025-08-24 22:28:38 +1000

    update applications, config and dashboard versions for kubernetes

--- kubernetes/kong/install.sh
+++ kubernetes/kong/install.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 kubectl apply -f "$SCRIPT_DIR/namespace.yaml"
 kubectl apply -f \
-  https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml
+  https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.1/standard-install.yaml
 kubectl apply -f "$SCRIPT_DIR/gatewayclass.yaml"
 kubectl apply -f "$SCRIPT_DIR/gateway.yaml"
 helm upgrade -i kong kong/ingress -n kong --values "$SCRIPT_DIR/values.yaml"
